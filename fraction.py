65ca53676ec0bbf5444486ec22ac5276b3a86ab2
malupaural <91567212+malupaural@users.noreply.github.com>
2021-09-28 15:55:04 -0400

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -2,3 +2,24 @@ def pgcd(a,b):
     while b:
         a,b = b,a%b
     return a
+
+class Fraction:
+    def __init__(self, n, d):
+        pass
+    
+    def simp(self):
+        pass
+    
+    def __add__(self,other):
+        pass
+    
+    def __sub__(self,other):
+        pass
+    
+    def __(self,other):
+        pass
+    
+    def divide(self,other):
+        pass
+    
+    def compare
