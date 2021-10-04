6cb2432e9c010032e4f2a31cbc634da64fd35b96
malupaural <91567212+malupaural@users.noreply.github.com>
2021-10-04 14:56:25 -0400

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -8,34 +8,47 @@ class Fraction:
         self.n = n
         self.d = d
         if d == 0:
-            raise ValueException("denomnateur egal a 0")
-    
+            raise ValueException("denominateur egal a 0")
+
     def simp(self):
-        if n < 0 and 
-    
+        if n < 0 and
+
     def __add__(self,other):
         pass
-    
+
     def __sub__(self,other):
         pass
-    
+
     def __mul__(self,other):
         pass
-    
+
     def __div__(self,other):
         pass
-    
+
     def __eq__(self,other):
-        pass
-    
+        1 = simp(Fraction(self.n*other.d, self.d * other.d))
+        2 = simp(Fractions(other.n*self.d, other.d * self.d))
+        if 1.n == 2.n :
+            return True
+        else:
+            return False
+
     def __ge__(self, other):
-        pass
-    
-    def __neg__(self, other):
-        pass
-    
+        1 = simp(Fraction(self.n*other.d, self.d * other.d))
+        2 = simp(Fractions(other.n*self.d, other.d * self.d))
+        if 1.n >= 2.n:
+            return self
+        elif 2.n >= 1.n:
+            return other
+        else:
+            return
+
+
+    def __neg__(self):
+        return Fraction(-self.n, self.d)
+
     def inv(self, others):
         pass
-    
+
     def puissance(self, others):
         pass
