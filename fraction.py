d07b6337dce5752ef87a88b72f0558a2a5095a07
malupaural <91567212+malupaural@users.noreply.github.com>
2021-10-04 15:04:16 -0400

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -10,34 +10,34 @@ class Fraction:
         if d == 0:
             raise ValueException("denominateur egal a 0")
 
-    
-    
+
+
     def simp(self):
         pgcdLocal = pgcd(self.n,self.d)
         self.n = self.n/pgcdLocal
         self.d = self.d/pgcdLocal
-    
+
     def __add__(self,other):
         self.n = self.n*other.d + other.n * self.d
         self.d = self.d * other.d
         self.simp()
-    
+
     def __sub__(self,other):
         self.n = self.n*other.d - other.n * self.d
         self.d = self.d * other.d
         self.simp()
-    
+
     def __mul__(self,other):
         self.n = self.n*other.n
         self.d = self.d*other.d
         self.simp()
-    
+
     def __div__(self,other):
         self.n = self.n*other.d
         self.d = self.d*other.n
         self.simp()
-    
-    
+
+
     def __eq__(self,other):
         1 = simp(Fraction(self.n*other.d, self.d * other.d))
         2 = simp(Fractions(other.n*self.d, other.d * self.d))
@@ -61,7 +61,7 @@ class Fraction:
         return Fraction(-self.n, self.d)
 
     def inv(self, others):
-        pass
+        return Fraction(self.d,self.n)
 
     def puissance(self, others):
         pass
